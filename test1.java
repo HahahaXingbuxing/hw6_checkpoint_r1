c2ca5df719fa11169c3e11adb07c2d5729b08871
Fiona <fionahu@Fionas-MacBook-Pro.local>
2019-04-25 22:17:34 -0400

    Last commit

--- test1.java
+++ test1.java
@@ -1,4 +1,6 @@
 // This is a test java file
 public class A {
-	
+	public A() {
+		
+	}
 }
--- test2.java
+++ test2.java
@@ -1,6 +1,6 @@
 // This is a test java file
 public class A {
 	public A() {
-		
+		System.out.println(" ");
 	}
 }
